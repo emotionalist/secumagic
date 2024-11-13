03773b32b6e09d574b2b94319d6454b81c90e5fb
eunbaji <kcorep@gmail.com>
2024-11-13 20:48:25 +0900

    delete

--- src/main/java/com/secure/secure/Hi.java
+++
@@ -1,4 +0,0 @@
-package com.secure.secure;
-
-public class Hi {
-}
--- src/main/java/com/secure/secure/helloworld.java
+++
@@ -1,7 +0,0 @@
-package com.secure.secure;
-
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("hello secure");
-    }
-}
